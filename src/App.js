57330ab98eedb2a6d6717185798af5d9f003a2af
cd-x <whowantsthisemailid@gmail.com>
2022-09-10 18:58:52 +0530

    able to fetch

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [dummyMovies, setDummyMovies] = useState([]);
+
+  const btnClickHandler = (event) => {
+    event.preventDefault();
+    const url = `https://swapi.dev/api/films`;
+    try {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          const transformedMovies = data.results.map((movie) => {
+            return {
+              id: movie["episode_id"],
+              title: movie.title,
+              openingText: movie.opening_crawl,
+              releaseDate: movie.release_date,
+            };
+          });
+          setDummyMovies(transformedMovies);
+        });
+    } catch {
+      console.log("Error fetching data !");
+    }
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={btnClickHandler}>Fetch Movies</button>
       </section>
       <section>
         <MoviesList movies={dummyMovies} />
