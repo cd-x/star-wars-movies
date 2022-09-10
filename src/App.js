fe1e1cf4d848f8ecf4c6b155ab87a90311dca904
cd-x <whowantsthisemailid@gmail.com>
2022-09-10 19:57:52 +0530

    call with useEffect and useCallback

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [dummyMovies, setDummyMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  const btnClickHandler = async () => {
+  const btnClickHandler = useCallback(async () => {
+    setIsError(false);
+    setIsLoading(true);
     const url = `https://swapi.dev/api/films`;
     const response = await fetch(url);
+    if (!response.ok) {
+      setIsError(true);
+    }
     const data = await response.json();
     const transformedMovies = data.results.map((movie) => {
       return {
-        id: movie["episode_id"],
+        id: movie.episode_id,
         title: movie.title,
         openingText: movie.opening_crawl,
         releaseDate: movie.release_date,
       };
     });
     setDummyMovies(transformedMovies);
-  };
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    btnClickHandler();
+  }, [btnClickHandler]);
 
   return (
     <React.Fragment>
@@ -27,7 +39,19 @@ function App() {
         <button onClick={btnClickHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {isError && (
+          <p>
+            Error Occured while fetching data{" "}
+            <span role="img" aria-label="Snowman">
+              &#9731;
+            </span>
+          </p>
+        )}
+        {isLoading && !isError && <p>Loading...</p>}
+        {!isLoading && !isError && dummyMovies.length === 0 && (
+          <p>No Movies Found !</p>
+        )}
+        {!isLoading && !isError && <MoviesList movies={dummyMovies} />}
       </section>
     </React.Fragment>
   );
